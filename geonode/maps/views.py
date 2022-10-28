cab68cd97ee2c59d2f30745a9966b83d0fb4cb9e
levohuutri <levohuutri@gmail.com>
2022-10-28 11:09:35 +0700

    click to view map detail in becamaps

--- geonode/maps/views.py
+++ geonode/maps/views.py
@@ -18,6 +18,7 @@
 #########################################################################
 import math
 import logging
+from operator import setitem
 import traceback
 from urllib.parse import quote, urlsplit, urljoin
 import warnings
@@ -171,6 +172,7 @@ def map_detail(request, mapid, template='maps/map_detail.html'):
             access_token = None
 
     context_dict = {
+        'becamaps_url': getattr(settings, 'BECAMAPS_URL'),
         'access_token': access_token,
         'config': config,
         'resource': map_obj,
--- geonode/settings.py
+++ geonode/settings.py
@@ -61,6 +61,8 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 # present pretty error pages.
 DEBUG = False # ast.literal_eval(os.getenv('DEBUG', 'True'))
 
+BECAMAPS_URL = os.getenv('BECAMAPS_URL', 'https://becamaps.vntts.vn')
+
 # Set to True to load non-minified versions of (static) client dependencies
 # Requires to set-up Node and tools that are required for static development
 # otherwise it will raise errors for the missing non-minified dependencies
